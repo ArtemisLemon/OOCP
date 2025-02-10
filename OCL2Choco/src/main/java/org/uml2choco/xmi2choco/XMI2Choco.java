26853c02b1e7b459fa0f1e96fe10eb25d643447b
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2025-02-10 11:24:23 +0100

    UMLCSP build logic

--- OCL2Choco/src/main/java/org/uml2choco/xmi2choco/XMI2Choco.java
+++ OCL2Choco/src/main/java/org/uml2choco/xmi2choco/XMI2Choco.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.*;
@@ -21,14 +22,15 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 public class XMI2Choco {
 
     static public void buildUMLCSP(EObject rootObj, EMFCSP csp){
-// TODO
+        System.out.println("Building UML CSP");
         int classCount = rootObj.eClass().getEReferences().size();
 
 
         // Identifiy the Objects in the Problem
-        System.out.println("\nLoading Objects\n");
+        System.out.println("Loading Objects");
 
         List<EClass> eclasses = new ArrayList<>();
+        List<EObject> eobjects = new ArrayList<>();
         HashMap<EClass,EList<EObject>> class2objects = new HashMap<>();
         Table<EClass,Integer,EObject> classptr2object = HashBasedTable.create();
 
@@ -43,33 +45,85 @@ public class XMI2Choco {
             
             //Make Pointers Table
             for(int j=0;j<iInstances.size();j++){
-                classptr2object.put(iClass,j,iInstances.get(j));
+                classptr2object.put(iClass,j+1,iInstances.get(j));
+                csp.addObjPtr(iInstances.get(j), j+1);
+                eobjects.add(iInstances.get(j));
             }
         }
+
+        // Identify the References between objects
+        System.out.println("Loading Links");
+        List<EReference> ereferences = new ArrayList<>();
+        HashMap<EClass,EList<EReference>> class2references = new HashMap<>();
+        Table<EObject,String,EList<EObject>> objref2objlist = HashBasedTable.create(); //Rows are strings, because that's the all the info the OCL gives us, could problably do a bit of typing in the future
+        // Table<EClass,EReference,RefTableInfo> classref2reftableinfo = HashBasedTable.create();
+
+        for(EClass c : eclasses){
+            // System.out.println("Class: "+c+"\n");
+            class2references.put(c, c.getEReferences());
+            // System.out.println("References: "+class2references.get(c)+"\n");
+
+            for(EReference r : c.getEReferences()){
+                ereferences.add(r);
+                int src_count = class2objects.get(c).size();
+                int tgt_count = class2objects.get(r.getEReferenceType()).size();
+                System.out.println("Building RefTable for "+r.getName()+" : "+c.getName()+"->"+r.getEReferenceType().getName());
+                csp.addReferenceTable(r, src_count, tgt_count);
+                
+                // classref2reftableinfo.put(c,r, info); //With this we can make the RefTables
+
+
+                //But now I want to save the data for every object, so that I can instanciate the RefTable values
+                //And pass the solution back
+                for(EObject o : class2objects.get(c)){
+                    // System.out.println("Object: "+o+"\nReference: "+r+"\n -> "+o.eGet(r));
+                    // List<EObject> wrapper;
+                    Object towrap = o.eGet(r);
+                    EList<EObject> wrapper = new BasicEList<>();
+                    if(towrap==null){
+                        // System.out.println("Found a ref to null\n");
+                    } else if(towrap instanceof EObject){
+                        // System.out.println("Found a ref to a single Object\n");
+                        wrapper.add((EObject)towrap);
+                    } else if(towrap instanceof EList<?>){
+                        // System.out.println("Found a ref to a List of Objects\n");
+                        wrapper = (EList<EObject>)towrap;
+                    }
+                    objref2objlist.put(o, r.getName(), wrapper);
+                }
+            }
+        }
+
+        System.out.println("Applying Opposite");
+        List<EReference> appliedOpposite = new ArrayList<>();
+        for(EReference r : ereferences){
+            if(r.getEOpposite()!=null)
+                if(appliedOpposite.contains(r.getEOpposite())==false){
+                    System.out.println(r.getName()+" opposite "+r.getEOpposite().getName());
+                    ReferenceTable.Opposites(csp, csp.getRefTable(r.getName()), csp.getRefTable(r.getEOpposite().getName()));
+                    appliedOpposite.add(r);
+                }
+        }
+
+        System.out.println("Recording EMFCSP Objects");
+        for(EObject o : eobjects){
+            // System.out.println(o.eClass().getName()+csp.getObjPtr(o));
+            EList<EReference> erefs = class2references.get(o.eClass());
+            HashMap<EReference,EList<EObject>> ref2objects = new HashMap<>();
+            HashMap<EReference,ReferenceTable.AdjList> var2objects = new HashMap<>();
+            for(EReference r : erefs){
+                ref2objects.put(r,objref2objlist.get(o,r.getName()));
+                var2objects.put(r,csp.getRefTable(r.getName()).adjList(csp.getObjPtr(o)));
+            }
+            csp.addEMFCSPObject(o,erefs,ref2objects,var2objects);
+        }
+
+        System.out.println("Loading data for EMFCSP Objects");
+        for(EMFCSP.EMFCSPObject o : csp.getEMFCSPObjects()){
+            // System.out.println("Loading data for "+o.emfobject);
+            o.data2variables();
+        }
+
+        System.out.println("Building UML CSP Finished");
     }
-
-    // static public Context loadInstance(EObject root, Context ctxt){
-    //     // get .var() Declaration
-        
-    //     // get objects
-    //         // list of root contents EClasses
-        
-
-    //         // make <EClass, list<EObject> class2obj
-    //     // get properties
-    //         // list of properties for EClasses (according to .var())
-    //         // get cardinalities, opposites, containement, etc...
-        
-    //     // for class2obj, for Props
-    //         // compile_PropTables
-    //         // make <EClass, EObject, Prop>
-    //     // for RefxRef if opposite
-    //         // Opposite CSP
-
-    //     // add this to context
-    // }
-
-    //make <EObject, Property> 
-    // static public Context compile_PropTables(EObject o, Context c){}
-    
 }
