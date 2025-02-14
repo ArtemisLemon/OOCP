b8bb9cbca017299b800908a7aa3f634ea84ed52e
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2025-02-14 18:16:20 +0100

    load zoo if no arguments

--- OCL2Choco/src/main/java/org/uml2choco/CLI.java
+++ OCL2Choco/src/main/java/org/uml2choco/CLI.java
@@ -5,13 +5,13 @@ import com.beust.jcommander.*;
 public class CLI {
 
     @Parameter(names = { "-M", "--metamodel" }, description = "path/to/MetaModel.ecore")
-    public String metamodel;
+    public String metamodel = "testmodels/zoo/zoo.ecore";
 
     @Parameter(names = { "-m", "--model" }, description = "path/to/Model.xmi")
-    public String model;
+    public String model = "testmodels/zoo/zoo.xmi";
 
     @Parameter(names = { "-c", "--ocl" }, description = "path/to/ObjectConstraints.atl")
-    public String ocl;
+    public String ocl  = "testmodels/zoo/zoo.atl";
 
     static public void main(String[] args){
         CLI cli = new CLI();
