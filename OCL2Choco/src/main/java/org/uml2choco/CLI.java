208170eb510aeca24b953d3bedec751e33d5caca
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2025-02-15 05:44:50 +0100

    added output option, and option to quickly load test cases

--- OCL2Choco/src/main/java/org/uml2choco/CLI.java
+++ OCL2Choco/src/main/java/org/uml2choco/CLI.java
@@ -13,15 +13,36 @@ public class CLI {
     @Parameter(names = { "-c", "--ocl" }, description = "path/to/ObjectConstraints.atl")
     public String ocl  = "testmodels/zoo/zoo.atl";
 
+    @Parameter(names = { "-o", "--out" }, description = "path/to/ObjectConstraints.atl")
+    public String out  = "solution.xmi";
+
+    @Parameter(names = { "-t", "--test" }, description = "testmodels/TEST")
+    public String test = "zoo";
+
     static public void main(String[] args){
         CLI cli = new CLI();
         JCommander.newBuilder().addObject(cli).build().parse(args);
 
+        switch (cli.test) {
+            case "rms":
+                cli.metamodel = "testmodels/rms/rms.ecore";
+                cli.model = "testmodels/rms/rms.xmi";
+                cli.ocl = "testmodels/rms/rms.atl";
+                cli.out = "testmodels/rms/solution.xmi";
+                break;
+            
+            case "zoo":
+                cli.out = "testmodels/zoo/solution.xmi";
+            default:
+                break;
+        }
+
+        
         System.out.println(cli.metamodel);
         System.out.println(cli.model);
         System.out.println(cli.ocl);
 
         EMF2Choco compiler = new EMF2Choco();
-        compiler.run(cli.metamodel,cli.model,cli.ocl);
+        compiler.run(cli.metamodel,cli.model,cli.ocl,cli.out);
     }
 }
