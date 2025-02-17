4828fecceab5895a2816299d002fdecf2aed0c78
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2025-02-17 13:48:43 +0100

    simplified using super data, and cleaned

--- OCL2Choco/src/main/java/org/uml2choco/EMFCSP.java
+++ OCL2Choco/src/main/java/org/uml2choco/EMFCSP.java
@@ -5,6 +5,7 @@ import org.oclinchoco.navigation.NavTable;
 import org.oclinchoco.property.AttributeTable;
 import org.oclinchoco.property.ReferenceTable;
 import org.oclinchoco.property.SingleIntTable;
+import org.uml2choco.utilities.PropertyWrapper;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
@@ -13,7 +14,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.chocosolver.solver.variables.IntVar;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
@@ -23,32 +23,19 @@ import org.eclipse.emf.ecore.EReference;
 
 //This class maps an EMF instance to an oclinchoco.CSP
 public class EMFCSP extends CSP{
-    // HashMap<String,EReference> nav2ref; //from NavOrAttribCallExp.getName()
-    // HashMap<EReference,ReferenceTable> referencetables;
-    List<ReferenceTable> justthetables;
-    List<String> tablenames;
-
-    //OCL View
-    HashMap<String,ReferenceTable> referencetables; // NavOrAttribCallExp.getName() -> ReferenceTable
+    
     HashMap<EObject,Integer> objPtrVals; //EObject -> int, useful when makeing self Node
-
-    HashMap<String,AttributeTable> attributetables;
-    HashMap<String,SingleIntTable> singleIntTable;
-
     HashMap<String,NavTable> navtables;
+    
     //UML View
     List<EMFCSPObject> objects;
     Table<EClass,Integer,EObject> classPtr2Obj;
 
     public EMFCSP(){
-        referencetables = new HashMap<>();
-        attributetables = new HashMap<>();
-        singleIntTable = new HashMap<>();
         navtables = new HashMap<>();
         objPtrVals = new HashMap<>();
 
         objects = new ArrayList<>();
-        tablenames = new ArrayList<>();
         classPtr2Obj = HashBasedTable.create();
     }
 
@@ -63,139 +50,144 @@ public class EMFCSP extends CSP{
     }
     
     public ReferenceTable getRefTable(String prop){
-        return referencetables.get(prop);
+        return (ReferenceTable)getTable(prop);
     }
 
     public NavTable getNavTable(String prop){
         // return referencetables.get(nav2ref.get(prop));
-        return navtables.get(prop);
+        return getTable(prop);
     }
 
     public void addSingleIntTable(EAttribute eattrib, int rows){
         SingleIntTable attribtable = new SingleIntTable(this, rows);
-        System.out.println(eattrib.getName()+" "+attribtable);
-        tablenames.add(eattrib.getName());
-        navtables.put(eattrib.getName(), attribtable);
-        singleIntTable.put(eattrib.getName(), attribtable);
-        System.out.println(eattrib.getName()+" "+attributetables.get(eattrib.getName()));
+        addTable(eattrib.getName(), attribtable);
     }
 
-    public SingleIntTable getSingleIntTable(String prop){
-        return singleIntTable.get(prop);
-    }
+    // public SingleIntTable getSingleIntTable(String prop){
+    //     return singleIntTable.get(prop);
+    // }
 
     public void addAttributeTable(EAttribute eAttribute, int rows, int minCard, int maxCard){
         AttributeTable attribtable = new AttributeTable(this, rows, minCard, maxCard);
+        addTable(eAttribute.getName(), attribtable);
     }
 
-    public AttributeTable getAttribTable(String prop){
-        return attributetables.get(prop);
-    }
+    // public AttributeTable getAttribTable(String prop){
+    //     return attributetables.get(prop);
+    // }
 
     public void addReferenceTable(EReference eref,int src_count, int tgt_count){
-        // System.out.println("EMFCSP: addReferenceTable");
         ReferenceTable refTable = new ReferenceTable(this,src_count,eref.getUpperBound(),eref.getLowerBound(),tgt_count);
-        // nav2ref.put(eref.getName(),eref);
-        // referencetables.put(eref, refTable);
-        tablenames.add(eref.getName());
-        navtables.put(eref.getName(), refTable);
-        referencetables.put(eref.getName(),refTable);
+        addTable(eref.getName(),refTable);
     }
 
     public class EMFCSPObject {
         public EObject emfobject;
         public EList<EReference> references;
-        public HashMap<EReference,EList<EObject>> links; //File Data
-        public HashMap<EReference, ReferenceTable.AdjList> link_variables; //CSP Data
-        
         public EList<EAttribute> attributes;
-        public HashMap<EAttribute,Integer> fields; //File Data
-        public HashMap<EAttribute, SingleIntTable.SingleIntAttribute> field_variables; //CSP Data
 
         public EClass getEClass(){return emfobject.eClass();}
         public EObject getEObject(){return emfobject;}
 
-        private EMFCSPObject(EObject e,
-        EList<EReference> r,HashMap<EReference,EList<EObject>> l,HashMap<EReference, ReferenceTable.AdjList> v,
-        EList<EAttribute> a, HashMap<EAttribute,Integer> al,HashMap<EAttribute, SingleIntTable.SingleIntAttribute> av){
+        private EMFCSPObject(EObject e, EList<EReference> r, EList<EAttribute> a){
             this.emfobject=e;
             this.references=r;
-            this.links=l;
-            this.link_variables=v;
-
             this.attributes=a;
-            this.fields=al;
-            this.field_variables=av;
         }
+
+        EList<EObject> getReferenceLinks(EReference r){
+            return PropertyWrapper.Links(emfobject, r);
+        }
+
+        EList<Integer> getAttributeValues(EAttribute a){
+            return PropertyWrapper.Ints(emfobject, a);
+        }
+
+        ReferenceTable.AdjList getReferenceVars(EReference r){
+            return ((ReferenceTable)getTable(r.getName())).adjList(getObjPtr(emfobject));
+        }
+
+        AttributeTable.AttributeTableRow getAttributeVars(EAttribute a){
+            return ((AttributeTable)getTable(a.getName())).getAttribute(getObjPtr(emfobject));
+        }
+
 
         public void data2variables(boolean verifMode){
             int bufferEmpty;
+
             for(EReference r : references){
                 bufferEmpty=-1;
                 if(verifMode) bufferEmpty=0;
-                EList<EObject> data = links.get(r);
-                // System.out.println("Loading data for "+r.getName());
-                ReferenceTable.AdjList variables = link_variables.get(r);
+
+                EList<EObject> data = getReferenceLinks(r);
+                if(data.size()==0) continue;
+
+                ReferenceTable.AdjList variables = getReferenceVars(r);
                 int cols = variables.size();
                 int[] dataBuffer = new int[cols];
-                for(int i=0;i<cols;i++) dataBuffer[i]=bufferEmpty;//-1
-
-                // System.out.println("Data to Load: "+data);
+                for(int i=0;i<cols;i++) dataBuffer[i]=bufferEmpty;
+                
                 for(int i=0;i<data.size();i++) {
-                    // System.out.println("Ptr Val: "+objPtrVals.get(data.get(i)));
                     dataBuffer[i]=objPtrVals.get(data.get(i));
                 }
+                
                 variables.loadData(dataBuffer);
-
             }
 
             for(EAttribute a : attributes){
                 bufferEmpty=CSP.MIN_BOUND-1;
                 if(verifMode) bufferEmpty=CSP.MIN_BOUND;
-                // System.out.println("Loading "+a.getName());
-                int[] data = {fields.get(a)};
-                SingleIntTable.SingleIntAttribute variable = field_variables.get(a);
-                variable.loadData(data);
+                
+                EList<Integer> data = getAttributeValues(a);
+                if(data.size()==0) continue;
+
+                AttributeTable.AttributeTableRow variables = getAttributeVars(a);
+                int cols = variables.size();
+                int[] dataBuffer = new int[cols];
+                for(int i=0;i<cols;i++) dataBuffer[i]=bufferEmpty;
+                
+                for(int i=0;i<data.size();i++) {
+                    dataBuffer[i]=data.get(i);
+                }
+
+                variables.loadData(dataBuffer);
             }
         }
+
+
         public void variables2data(){
-            // System.out.println("moving data from solver to instance");
             for(EReference r : references){
-                // System.out.println("Reference "+r.getName()+":"+r.getEReferenceType().getName());
                 EList<EObject> buff = new BasicEList<>();
-                int[] data = link_variables.get(r).getData();
+                int[] data = getReferenceVars(r).getData();
                 for(int i=0; i<data.length;i++){
                     if(data[i]==nullptr().getValue()) continue;
                     buff.add(classPtr2Obj.get(r.getEReferenceType(), data[i]));
                 }
-                // System.out.println(buff);
-                // for(EObject o : buff) System.out.println(o);
-
                 if(!buff.isEmpty()){
                     if(r.getUpperBound()==1) emfobject.eSet(r, buff.get(0));
                     else emfobject.eSet(r, buff);
                 }
             }
+            for(EAttribute a : attributes){
+                EList<Integer> buff = new BasicEList<>();
+                int[] data = getAttributeVars(a).getData();
+                for(int i=0; i<data.length;i++){
+                    if(data[i]==nullattrib().getValue()) continue;
+                    buff.add(data[i]);
+                }
+                if(!buff.isEmpty()){
+                    if(a.getUpperBound()==1) emfobject.eSet(a, buff.get(0));
+                    else emfobject.eSet(a, buff);
+                }
+            }
         }
     }
 
-    public void addEMFCSPObject(EObject e,
-    EList<EReference> r,HashMap<EReference,EList<EObject>> l,HashMap<EReference, ReferenceTable.AdjList> v,
-    EList<EAttribute> a, HashMap<EAttribute,Integer> al,HashMap<EAttribute, SingleIntTable.SingleIntAttribute> av){
-        objects.add(new EMFCSPObject(e, r, l, v, a, al, av));
+    public void addEMFCSPObject(EObject e, EList<EReference> r, EList<EAttribute> a){
+        objects.add(new EMFCSPObject(e, r, a));
     }
 
     public List<EMFCSPObject> getEMFCSPObjects(){
         return objects;
-    }
-
-    public void printJustTheTables(){
-        for(String r : tablenames){
-            Object out = referencetables.get(r);
-            if(out==null) out = attributetables.get(r);
-            if(out==null) out = singleIntTable.get(r);
-            System.out.println(r);
-            System.out.println(out);
-        }
     }
 }
